04764770bd0412bb7dfcedb58162c528888c4383
User <dambert.albano.scc@gmail.com>
2024-03-07 14:13:38 +0800

    xD

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/EmployeeMS/",
-  plugins: [react()]
+  plugins: [react()],
+  base: "/EmployeeMS/"
 })
